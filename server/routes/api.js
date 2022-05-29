fd54dd146499909dc0ee791ad59e4544577d2aa0
dekork <deniskrkt@gmail.com>
2022-05-28 19:42:35 -0700

    Change sql table to have boat names unique. Added new editboat api call (TODO: add test for this). Added sending back id to addboat api call for tests. Added addboat test.

--- server/routes/api.js
+++ server/routes/api.js
@@ -2,8 +2,7 @@ var express = require('express');
 var router = express.Router();
 var connection = require('../config/database');
 
-/* GET home page. */
-//Using this for manual testing
+// Lists all boats as JSON
 router.get('/list', function(req, res, next) {
     var boatslist = connection.query('SELECT * FROM boats', function (error, results, fields) {
         if (error) throw error;
@@ -11,6 +10,8 @@ router.get('/list', function(req, res, next) {
     });
 });
 
+// Adds a boat with default swimlane 0 (Docked)
+//  Inputs: req.bodyboatname, req.body.operatorname -- **TODO: change this after building frontend
 router.post('/addboat', function(req, res, next)
 {
     var boat = req.body.boatname;
@@ -25,9 +26,33 @@ router.post('/addboat', function(req, res, next)
            res.sendStatus(400);
            return;
         }
-        res.sendStatus(200);
+        res.send(results.insertId.toString());
         });
 
+});
+
+// Edit an existing boat Ex: /editboat/1
+//  Input: id (via URL), req.body.boatname, req.body.operatorname
+// TODO: add test
+router.post('/editboat/:id', function(req, res) {
+    var id = req.params.id;
+    var vesselname = req.body.vesselname;
+    var operator = req.body.operatorname;
+    var swimlane = req.body.swimlane;
+    connection.query('UPDATE boats' +
+        'SET vessel_name=?, operator_name=?, swimlane=?' +
+        'WHERE id = ?', [vesselname, operator, swimlane, id], function(error, results) {
+        if (error) {
+            res.sendStatus(400);
+            return;
+        }
+        if (results.affectedRows == 0)
+        {
+            res.sendStatus(400);
+            return;
+        }
+        res.sendStatus(200);
+    });
 });
 
 module.exports = router;
--- server/routes/api.test.js
+++ server/routes/api.test.js
@@ -1,10 +1,27 @@
 const app = require("../app");
+const connection = require("../config/database")
 const request = require("supertest");
 
-describe("GET /", () => {
+// Simple test of getting full list of boats from api
+describe("GET /api/list", () => {
     test("Gets the full list of boats", async () => {
-        return request(app).get("/").expect(200);
+        return request(app).get("/api/list").expect(200);
     });
 });
+
+// Test adding a boat, then remove the added boat in sql to keep data unaltered.
+describe("POST /api/addboat", () => {
+    test("Add a boat successfully then delete manually in SQL", async () => {
+        return request(app)
+            .post("/api/addboat")
+            .send({boatname: "Test2boat352624", operatorname: "Oper@tor3250823"})
+            .set('Accept', 'application/json')
+            .expect(200)
+            .then((res) => {
+                connection.query('DELETE from boats WHERE id=?',res.text);
+            });
+    });
+});
+
 
 
