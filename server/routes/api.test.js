450f6c425a3c37510e664a66b055ab1c94d210b9
dekork <deniskrkt@gmail.com>
2022-05-30 21:21:59 -0700

    Add tests for all remaining endpoints.

--- server/routes/api.test.js
+++ server/routes/api.test.js
@@ -12,17 +12,59 @@ describe("GET /api/list", () => {
 // Test adding a boat, then remove the added boat in sql to keep data unaltered.
 describe("POST /api/addboat", () => {
     test("Add a boat successfully then delete manually in SQL", async () => {
-        return request(app)
-            .post("/api/addboat")
-            .send({vessel_name: "Test2boat352624", operator_name: "Oper@tor3250823"})
+        return request(app).post("/api/addboat")
+            .send({vessel_name: "Test2boat3dfas52624", operator_name: "Oper@tor3250dfs823"})
             .set('Accept', 'application/json')
             .expect(200)
             .then((res) => {
-                connection.query('DELETE from boats WHERE id=?',res.text);
+                connection.query('DELETE from boats WHERE id=?', res.text);
             });
     });
 });
 
-//TODO: add test for /editboat/:id
+
+describe("POST /api/editboat/:id", () => {
+    test("Add a boat successfully then edit, then delete manually in SQL", async () => {
+        request(app).post("/api/addboat")
+            .send({vessel_name: "Test2boat352ewrt624", operator_name: "Oper@tor32trew50823"})
+            .set('Accept', 'application/json').then((res) => {
+            let id = res.text;
+            request(app).post("/api/editboat/" + id).send(
+                {vessel_name: "simplerboatname123", operator_name: "Operatornotypo"})
+                .set('Accept', 'application/json')
+                .expect(200).then((res) => {
+                connection.query('SELECT * from boats WHERE id=' + id, function (error, results) {
+                    if (error)
+                        fail('SQL error');
+                    if (results.vessel_name !== "simplerboatname123" || results.operator_name !== "Operatornotypo") {
+                        fail('The edited data was not stored correctly!');
+                    }
+                    connection.query('DELETE from boats WHERE id=' + id);
+                });
+            });
+        });
+    });
+});
+
 
 //TODO: add test for /deleteboat/:id
+describe("POST /api/deleteboat/:id", () => {
+    test("Add a boat successfully then delete", async () => {
+
+        request(app).post("/api/addboat")
+            .send({vessel_name: "Test2boat3trew5123123422", operator_name: "Oper@tor325082222243213"})
+            .set('Accept', 'application/json').then((res) => {
+            let id = res.text;
+            request(app).post("/api/deleteboat/" + id).send()
+                .expect(200).then((res) => {
+                connection.query('SELECT * from boats WHERE id=' + id, function (error, results) {
+                    if (error)
+                        fail('SQL error');
+                    console.log("RESULTS: ");
+                    console.log(results);
+                });
+            });
+        });
+    });
+});
+
